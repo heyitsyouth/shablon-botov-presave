de30f76bc13b24666dd1547f6ef2ff3071760fb9
nikulin ivan <163635418+heyitsyouth@users.noreply.github.com>
2026-07-06 23:26:09 +0300

    Create __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,13 @@
+from .keyboards import (
+    get_start_keyboard,
+    get_subscription_keyboard,
+    get_admin_keyboard,
+    get_cancel_keyboard,
+)
+
+__all__ = [
+    "get_start_keyboard",
+    "get_subscription_keyboard",
+    "get_admin_keyboard",
+    "get_cancel_keyboard",
+]
