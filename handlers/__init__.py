538c1fbaf6cf9520573f1214065b6314ad730f97
nikulin ivan <163635418+heyitsyouth@users.noreply.github.com>
2026-07-06 23:40:20 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,9 @@
 from .start import router as start_router
+from .screenshots import router as screenshots_router
+from .callbacks import router as callbacks_router
 
 __all__ = [
     "start_router",
+    "screenshots_router",
+    "callbacks_router",
 ]
