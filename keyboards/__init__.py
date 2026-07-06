f7ae8f80405db63ded15dd992a343fe32b49621f
nikulin ivan <163635418+heyitsyouth@users.noreply.github.com>
2026-07-07 01:31:07 +0300

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,13 +1,11 @@
 from .keyboards import (
-    get_start_keyboard,
-    get_subscription_keyboard,
     get_admin_keyboard,
     get_cancel_keyboard,
+    get_start_keyboard,
 )
 
 __all__ = [
-    "get_start_keyboard",
-    "get_subscription_keyboard",
     "get_admin_keyboard",
     "get_cancel_keyboard",
+    "get_start_keyboard",
 ]
